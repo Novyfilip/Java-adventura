df6fd5111378543a421d41b955be7bf1319ad57e
Filip Nový <novf02@vse.cz>
2023-11-05 19:02:51 +0100

    Úprava pom pro verzi 0.4 a gitlab-ci config

--- src/main/java/cz/vse/novf02/main/HomeController.java
+++ src/main/java/cz/vse/novf02/main/HomeController.java
@@ -6,11 +6,14 @@ import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.util.Duration;
 
+import java.util.Optional;
+
 public class HomeController {
+    @FXML
+    private Button tlacitkoOdesli;
     @FXML
     private TextField vstup;
     @FXML
@@ -42,9 +45,18 @@ public class HomeController {
         if (game.gameEnd()) {
             String epilog = game.returnEpilog();
             vystup.appendText(epilog + "\n\n");
+            vstup.setDisable(true);
+            tlacitkoOdesli.setDisable(true);
             // Zavře hru
             closeWindow();
         }
         vstup.clear();
+    }
+
+    public void ukončitHru(ActionEvent actionEvent) {
+        Alert alert = new Alert(javafx.scene.control.Alert.AlertType.CONFIRMATION,"Opravdu chcete zavřít hru?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK)
+        Platform.exit();
     }
 }
